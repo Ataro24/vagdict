130aca2eaa7d3f32c457f6c637ca3e68dfda6391
Ataro24 <touch.taro@gmail.com>
2014-06-21 23:49:37 +0900

    first commit and add interface

---
+++ support.rb
@@ -0,0 +1,28 @@
+$LOAD_PATH << File.dirname(__FILE__)
+require 'ipsupport'
+
+module VagrantSupport
+
+class Supporter
+  def self.get_vmname()
+    here = Dir::pwd
+    name = here.split("/").last
+    p here, name
+    name
+  end
+
+  def self.get_ip(vmname, conf)
+    IpSupport.new(conf).load_ipaddr(vmname)
+  end
+
+  def self.get_boxurl(boxname)
+  end
+
+  def conf_exist?(path)
+    File.exist?(path)
+  end
+
+
+end
+
+end
